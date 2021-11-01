a3b974f298f2918d783817c5d95d44d9abbd4207
vladislav <ve@incsub.com>
2021-11-01 18:11:07 +0100

    Implement better tests

--- processor/lexer_test.go
+++ processor/lexer_test.go
@@ -3,14 +3,25 @@ package processor
 import (
 	"testing"
 	"strings"
+
+	"mage/typedefs"
 )
 
 func Test_LexFile(t *testing.T) {
 	lines, _ := loadFile("../fixtures/macro.mg")
 	tokens := xlex("test", strings.Join(lines, "\n"))
-	// if 10 != len(tokens) {
-	// 	t.Fatalf("expected 10 tokens")
-	// }
+	expected := 63
+	if expected != len(tokens) {
+		t.Fatalf("expected %d tokens, but got %d", expected, len(tokens))
+	}
+	tokMacros := filterTokens(tokens, typedefs.TOKEN_MACRO_DFN_OPEN)
+	if 5 != len(tokMacros) {
+		t.Fatalf("there should be 5 macros, not %d", len(tokMacros))
+	}
+	tokRules := filterTokens(tokens, typedefs.TOKEN_RULE_OPEN)
+	if 2 != len(tokRules) {
+		t.Fatalf("there should be 2 rule dfns, not %d", len(tokRules))
+	}
 	for _, tk := range tokens {
 		t.Log(tk)
 	}
