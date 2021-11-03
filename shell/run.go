f4c70e19c79ac7c260ef13af6be4aec9639e2380
vladislav <ve@incsub.com>
2021-11-03 18:43:00 +0100

    Add command running abstraction layer

---
+++ shell/run.go
@@ -0,0 +1,41 @@
+package shell
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+type Command struct {
+	raw      []string
+	executed bool
+	out      []byte
+}
+
+func NewCommand(cmd []string) *Command {
+	return &Command{cmd, false, []byte{}}
+}
+
+func (c *Command) GetStdout() (string, error) {
+	if c.executed {
+		return string(c.out[:]), nil
+	}
+	err := c.Run()
+	if err != nil {
+		return "", err
+	}
+	return c.GetStdout()
+}
+
+func (c *Command) Run() error {
+	c.executed = true
+	if len(c.raw) < 1 {
+		return fmt.Errorf("not enough arguments for command")
+	}
+	args := []string{"-c"}
+	out, err := exec.Command("sh", append(args, c.raw...)...).Output()
+	if err != nil {
+		return err
+	}
+	c.out = out
+	return nil
+}
---
+++ shell/run_test.go
@@ -0,0 +1,51 @@
+package shell
+
+import (
+	"fmt"
+	"testing"
+)
+
+type TestAlwaysFailCommand struct {
+	Command
+}
+
+func (tafc TestAlwaysFailCommand) Run() error {
+	return fmt.Errorf("always fail")
+}
+
+func Test_RunCommand(t *testing.T) {
+	tafc := TestAlwaysFailCommand{}
+	err := tafc.Run()
+	if err == nil {
+		t.Fatalf("expected to fail")
+	}
+
+	empty := NewCommand([]string{})
+	err = empty.Run()
+	if err == nil {
+		t.Fatalf("empty command should error")
+	}
+
+	invalid := NewCommand([]string{"fdgdfgvcxvbxcvb", "cxzbxchthgf"})
+	err = invalid.Run()
+	if err == nil {
+		t.Fatalf("invalid command should error")
+	}
+
+	invalid2 := NewCommand([]string{"fdgdfgvcxvbxcvb", "cxzbxchthgf"})
+	_, err = invalid2.GetStdout()
+	if err == nil {
+		t.Fatalf("invalid command should error on getting output")
+	}
+
+	ls := NewCommand([]string{"ls"})
+	out, err := ls.GetStdout()
+	if err != nil {
+		t.Log(err)
+		t.Fatal("ls command should succeed")
+	}
+
+	if len(out) <= 0 {
+		t.Fatalf("expected ls to return files list")
+	}
+}
