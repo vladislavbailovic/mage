c74bb59b29ccede23e74dc5381195af490978d29
vladislav <ve@incsub.com>
2021-11-05 06:35:16 +0100

    Start cleaning up

--- processing/includes_test.go
+++ processing/includes_test.go
@@ -15,21 +15,6 @@ func Test_TokenizeIncludes(t *testing.T) {
 	}
 }
 
-// func Test_PreprocessIncludes(t *testing.T) {
-// 	filepath := "../fixtures/includes.mg"
-// 	lines, _ := shell.LoadFile(filepath)
-// 	tkn := newTokenizer(filepath, lines)
-// 	rawTokens := tkn.tokenize()
-// 	tokens, err := preprocessIncludes(rawTokens)
-// 	if err != nil {
-// 		t.Fatalf("preprocessing includes error: %s", err)
-// 	}
-// 	// debug.Tokens(tokens)
-// 	if 128 != len(tokens) {
-// 		t.Fatalf("expected exactly 128 tokens with includes, got %d", len(tokens))
-// 	}
-// }
-
 func Test_PreprocessorDoesIncludes(t *testing.T) {
 	filepath := "../fixtures/includes.mg"
 	lines, _ := shell.LoadFile(filepath)
@@ -44,28 +29,6 @@ func Test_PreprocessorDoesIncludes(t *testing.T) {
 		t.Fatalf("expected exactly 128 tokens with includes, got %d", len(proc.tokens))
 	}
 }
-
-// func Test_ApplyIncludes(t *testing.T) {
-// 	filepath := "../fixtures/includes.mg"
-// 	lines, _ := shell.LoadFile(filepath)
-// 	tkn := newTokenizer(filepath, lines)
-// 	rawTokens, err := preprocessIncludes(tkn.tokenize())
-// 	if err != nil {
-// 		t.Log(err)
-// 		t.Fatalf("includes preprocessing should have been a success")
-// 	}
-
-// 	tokens, err := preprocessMacros(rawTokens)
-// 	if err != nil {
-// 		t.Log(err)
-// 		t.Fatalf("macros expansion failed")
-// 	}
-
-// 	// debug.Tokens(tokens)
-// 	if 102 != len(tokens) {
-// 		t.Fatalf("expected exactly 102 tokens with includes, got %d", len(tokens))
-// 	}
-// }
 
 func Test_PreprocessorAppliesIncludes(t *testing.T) {
 	filepath := "../fixtures/includes.mg"
@@ -106,12 +69,13 @@ func Test_MultilevelInclusionShouldWork(t *testing.T) {
 	filepath := "../fixtures/double-include-parent.mg"
 	lines, _ := shell.LoadFile(filepath)
 	tkn := newTokenizer(filepath, lines)
-	tokens, err := preprocessIncludes(tkn.tokenize())
+	proc := NewPreprocessor(tkn.tokenize())
+	err := proc.doIncludes()
 	if err != nil {
 		t.Log(err)
 		t.Fatalf("multi-level include should work")
 	}
-	if len(tokens) != 87 {
-		t.Fatalf("expected exactly 87 tokens in multi-level include, got %d", len(tokens))
+	if len(proc.tokens) != 87 {
+		t.Fatalf("expected exactly 87 tokens in multi-level include, got %d", len(proc.tokens))
 	}
 }
