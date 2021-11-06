f427c5a4d3b2d2956059e0d1a77372479e17bed3
vladislav <ve@incsub.com>
2021-11-06 06:36:21 +0100

    Improve tests

--- processing/processor_test.go
+++ processing/processor_test.go
@@ -25,3 +25,23 @@ func Test_Process_RedefiningRulesCausesError(t *testing.T) {
 		t.Fatalf("expected rule name conflict")
 	}
 }
+
+func Test_Process_GetFirstRule_ErrorsOnEmptyStack(t *testing.T) {
+	proc := NewProcessor("../fixtures/includes.mg")
+	_, err := proc.GetFirstTaskName()
+	if err == nil {
+		t.Fatalf("expected to error out if asked for first task before processing everything")
+	}
+}
+
+func Test_Process_GetFirstRule_ResolvesFirstRule(t *testing.T) {
+	proc := NewProcessor("../fixtures/includes.mg")
+	proc.GetTasks()
+	rule, err := proc.GetFirstTaskName()
+	if err != nil {
+		t.Fatalf("expected rule to resolve")
+	}
+	if rule != "conflict-rule" { // because inclusion comes first
+		t.Fatalf("expected first task to be 'conflict-rule', got %s", rule)
+	}
+}
